66f5db0f89cec8b2ead2affbf7e3a59e9712dd79
JiaMing746 <3961215806@qq.com>
2024-06-11 13:32:55 +0800

    Add files via upload

--- app/src/main/java/com/Sunshine/ToolBox/activities/ActivityStartSplash.kt
+++ app/src/main/java/com/Sunshine/ToolBox/activities/ActivityStartSplash.kt
@@ -151,7 +151,7 @@ class ActivityStartSplash : Activity() {
     private class InstallBusybox(private val context: ActivityStartSplash) : Runnable {
         override fun run() {
             context.start_state_text.text = "检查Busybox是否安装..."
-           
+           startToFinish()
         }
     }
 
